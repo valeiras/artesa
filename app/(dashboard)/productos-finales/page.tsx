aee4194061d1763e61d68ca2e0b972cde7bf62ae
David Reverter Valeiras <d.r.valeiras@gmail.com>
2024-05-03 07:48:58 +0200

    Removes the old new page

--- app/(dashboard)/productos-finales/page.tsx
+++ app/(dashboard)/productos-finales/page.tsx
@@ -1,13 +1,24 @@
 import React from "react";
-import underConstruction from "@/assets/under_construction.svg";
-import Image from "next/image";
 
-const ProductsPage: React.FC = () => {
+import { getAllProducts } from "@/lib/actions/productActions";
+import { dehydrate, HydrationBoundary, QueryClient } from "@tanstack/react-query";
+import ProductsDataTable from "@/components/products/ProductsDataTable";
+
+const ProductsPage: React.FC = async () => {
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchQuery({
+    queryKey: ["products"],
+    queryFn: () => getAllProducts(),
+  });
+
   return (
-    <div className="flex-flex-col">
-      <h2 className="text-4xl font-bold">Estamos trabajando en ello...</h2>
-      <Image src={underConstruction} alt="Trabajo en curso" className="w-1/2 mt-16" />
-    </div>
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <div className="flex flex-row justify-between items-center mb-8">
+        <h2 className="item-list-header">Clientes:</h2>
+      </div>
+      <ProductsDataTable />
+    </HydrationBoundary>
   );
 };
 
