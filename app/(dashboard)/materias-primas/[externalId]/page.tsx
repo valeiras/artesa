597aabe5989c2a439002a209dc81e0221833af72
David Reverter Valeiras <d.r.valeiras@gmail.com>
2024-05-10 07:43:50 +0200

    Centers the under construction page

--- app/(dashboard)/materias-primas/[externalId]/page.tsx
+++ app/(dashboard)/materias-primas/[externalId]/page.tsx
@@ -4,8 +4,8 @@ import Image from "next/image";
 
 const InventoryPage: React.FC = () => {
   return (
-    <div className="flex-flex-col">
-      <h2 className="text-4xl font-bold">Estamos trabajando en ello...</h2>
+    <div className="flex flex-col w-full items-center">
+      <h2 className="text-4xl font-bold text-center">Estamos trabajando en ello...</h2>
       <Image src={underConstruction} alt="Trabajo en curso" className="w-1/2 mt-16" />
     </div>
   );
