1e8c69eeacab3f89ca51802a06d404732aa8faf3
David Reverter Valeiras <d.r.valeiras@gmail.com>
2024-07-12 17:33:19 +0200

    Small adjustments to the landing

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <main className="flex flex-col min-h-screen items-center">
       <header className="lg:max-w-6xl w-full px-4 sm:px-8 py-6">
-        <Image src={logo} alt="logo" width={300} className="w-[200px] sm:w-[300px]" />
+        <Image src={logo} alt="logo" width={300} className="w-[250px] sm:w-[300px]" />
       </header>
       <section className="pt-8 lg:max-w-6xl w-full flex flex-col items-start px-4 sm:px-8 gap-16 sm:gap-4 flex-1 justify-center lg:grid lg:grid-cols-[1fr,400px] lg:justify-start lg:items-center">
         <div className="flex flex-col gap-6 items-start">
@@ -25,7 +25,7 @@ export default function Home() {
         <Image
           src={landingImg}
           alt="landing"
-          className="w-full max-w-[300px] sm:max-w-[400px] lg:w-auto mx-auto sm:mx-0 sm:ml-auto"
+          className="w-full max-w-[250px] sm:max-w-[400px] lg:w-auto mx-auto sm:mx-0 sm:ml-auto"
         />
       </section>
     </main>
