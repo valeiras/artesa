bfcd29406535a81bf19b511d087843e96413b914
David Reverter Valeiras <d.r.valeiras@gmail.com>
2024-07-15 17:43:50 +0200

    Removes the ScrollArea

--- components/CommentsContainer.tsx
+++ components/CommentsContainer.tsx
@@ -1,15 +1,10 @@
 import React from "react";
-import { ScrollArea } from "./ui/scroll-area";
 
 type Props = { comments: string | null };
 
 const CommentsContainer: React.FC<Props> = ({ comments }) => {
   if (!comments) return null;
-  return (
-    <ScrollArea className="max-w-48 h-fit p-2 rounded-md border" viewportClassName="max-h-[60px]">
-      {comments}
-    </ScrollArea>
-  );
+  return <div className="max-w-48 h-fit px-2">{comments}</div>;
 };
 
 export default CommentsContainer;
